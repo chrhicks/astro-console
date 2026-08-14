be26ff5ea85138aacd995ba01eb42317db9e9b61
Christopher Hicks <chrhicks@gmail.com>
2026-08-14 17:21:20 -0400

    refactor(web): narrow local UI interface

--- apps/web/src/components/ui/index.ts
+++ apps/web/src/components/ui/index.ts
@@ -1,53 +1,19 @@
 import './foundations/tokens.css'
 
 export type { ActionDescriptor, Tone } from './foundations/types'
-export { ActionPanel, type ActionPanelProps } from './ActionPanel'
-export {
-  AttentionCard,
-  AttemptTrail,
-  type AttentionCardProps,
-  type AttemptItem,
-  type AttemptTrailProps,
-} from './AttentionCard'
-export { Button, type ButtonProps } from './Button'
-export { DataList, DataListItem, type DataListItemProps } from './DataList'
-export {
-  EvidenceViewport,
-  type EvidenceViewportProps,
-} from './EvidenceViewport'
-export { Field, type FieldProps } from './Field'
-export {
-  Checkbox,
-  NumberField,
-  Select,
-  TextField,
-  type CheckboxProps,
-  type NumberFieldProps,
-  type SelectProps,
-  type TextFieldProps,
-} from './FormControls'
+export { ActionPanel } from './ActionPanel'
+export { AttentionCard, AttemptTrail, type AttemptItem } from './AttentionCard'
+export { Button } from './Button'
+export { DataList, DataListItem } from './DataList'
+export { EvidenceViewport } from './EvidenceViewport'
+export { Field } from './Field'
+export { Checkbox, NumberField, Select, TextField } from './FormControls'
 export { Cluster, Stack } from './Layout'
-export {
-  MetricOverlay,
-  type MetricOverlayItem,
-  type MetricOverlayProps,
-} from './MetricOverlay'
-export {
-  Dialog,
-  Flyout,
-  type DialogProps,
-  type FlyoutProps,
-  type FlyoutTriggerProps,
-} from './Overlays'
-export { PageHeader, type PageHeaderProps } from './PageHeader'
-export {
-  Panel,
-  PanelBody,
-  PanelHeader,
-  type PanelHeaderProps,
-  type PanelProps,
-} from './Panel'
-export { StatusIndicator, type StatusIndicatorProps } from './StatusIndicator'
-export { StepRail, type StepItem, type StepRailProps } from './StepRail'
-export { Tabs, type TabItem, type TabsProps } from './Tabs'
-export { Toolbar, type ToolbarProps } from './Toolbar'
+export { MetricOverlay } from './MetricOverlay'
+export { Dialog, Flyout, type FlyoutTriggerProps } from './Overlays'
+export { PageHeader } from './PageHeader'
+export { Panel, PanelBody, PanelHeader } from './Panel'
+export { StatusIndicator } from './StatusIndicator'
+export { StepRail, type StepItem } from './StepRail'
+export { Tabs } from './Tabs'
+export { Toolbar } from './Toolbar'
